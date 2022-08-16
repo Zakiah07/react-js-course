f74b452d5a435c4e0246cf064ff62b5c314b605e
Zakiah07 <zakiah_bloom@yahoo.com>
2022-08-16 12:34:36 +0800

    added ternary

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import "./App.css";
-import { User } from "./User";
+
+// function App() {
+//   const users = [
+//     { name: "Zakiah", age: 21 },
+//     { name: "Baem", age: 40 },
+//     { name: "Inara", age: 2 },
+//   ];
+
+//   return (
+//     <div className="App">
+//       {users.map((user, key) => {
+//         return <User name={user.name} age={user.age} />;
+//       })}
+//     </div>
+//   );
+// }
 
 function App() {
-  const users = [
-    { name: "Zakiah", age: 21 },
-    { name: "Baem", age: 40 },
-    { name: "Inara", age: 2 },
+  const planets = [
+    { name: "Mars", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true },
+    { name: "Venus", isGasPlanet: false },
+    { name: "Neptune", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
   ];
 
   return (
     <div className="App">
-      {users.map((user, key) => {
-        return <User name={user.name} age={user.age} />;
-      })}
+      {planets.map(
+        // (planet, key) => planet.isGasPlanet && <h1>{planet.name}</h1>
+        // if not gas planet
+        (planet, key) => !planet.isGasPlanet && <h1>{planet.name}</h1>
+      )}
     </div>
   );
 }
